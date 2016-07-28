0dac245ff767a5ada63ad24795add4de4544a5f0
Axel Pardemann <axelitusdev@gmail.com>
2016-07-28 11:35:20 -0500

    Added an InvalidCertificateException class

---
+++ src/Certificates/InvalidCertificateException.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Sikker is a PHP 7.0+ Security package that contains security related implementations.
+ *
+ * @package    NorseBlue\Sikker
+ * @version    0.2
+ * @author     NorseBlue
+ * @license    MIT License
+ * @copyright  2016 NorseBlue
+ * @link       https://github.com/NorseBlue/Sikker
+ */
+declare(strict_types = 1);
+
+namespace NorseBlue\Sikker\Certificates;
+
+use Exception;
+
+/**
+ * Class InvalidCertificateException
+ *
+ * @package NorseBlue\Sikker\Certificates
+ * @since 0.3
+ */
+class InvalidCertificateException extends Exception
+{
+}
