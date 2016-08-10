49e29afad657348f081f96255b8239b27a17ba04
Axel Pardemann <axelitusdev@gmail.com>
2016-08-10 11:37:04 -0500

    Fixed/added the tests in the KeyPairTest class

--- tests/unit/Asymmetric/KeyPairTest.php
+++ tests/unit/Asymmetric/KeyPairTest.php
@@ -67,6 +67,11 @@ class KeyPairTest extends Unit
      */
     const EC_PUBLIC_KEY_EXAMPLE_FILE = 'tests/_data/EC_public_key_example.pem';
 
+    /**
+     * @var string Helper payload to hash.
+     */
+    const PAYLOAD = 'You know nothing Jon Snow! Winter is coming!';
+
     protected function _after()
     {
     }
@@ -97,34 +102,102 @@ class KeyPairTest extends Unit
     public function testFromPEM()
     {
         $this->specify('Loads an RSA KeyPair instance from the keys PEM strings.', function () {
-            $privateKeyContents = str_replace("\r", "", trim(file_get_contents(self::RSA_PRIVATE_KEY_EXAMPLE_FILE)));
-            $publicKeyContents = str_replace("\r", "", trim(file_get_contents(self::RSA_PUBLIC_KEY_EXAMPLE_FILE)));
-            $keyPair = KeyPair::fromPEM($privateKeyContents, $publicKeyContents);
+            if (extension_loaded('openssl')) {
+                $privateKeyContents = str_replace("\r", "",
+                    trim(file_get_contents(self::RSA_PRIVATE_KEY_EXAMPLE_FILE)));
+                $publicKeyContents = str_replace("\r", "", trim(file_get_contents(self::RSA_PUBLIC_KEY_EXAMPLE_FILE)));
+                $keyPair = KeyPair::fromPEM($privateKeyContents, $publicKeyContents);
+            }
         });
 
         $this->specify('Loads an DSA KeyPair instance from the keys PEM strings.', function () {
-            $privateKeyContents = str_replace("\r", "", trim(file_get_contents(self::DSA_PRIVATE_KEY_EXAMPLE_FILE)));
-            $publicKeyContents = str_replace("\r", "", trim(file_get_contents(self::DSA_PUBLIC_KEY_EXAMPLE_FILE)));
-            $keyPair = KeyPair::fromPEM($privateKeyContents, $publicKeyContents);
+            if (extension_loaded('openssl')) {
+                $privateKeyContents = str_replace("\r", "",
+                    trim(file_get_contents(self::DSA_PRIVATE_KEY_EXAMPLE_FILE)));
+                $publicKeyContents = str_replace("\r", "", trim(file_get_contents(self::DSA_PUBLIC_KEY_EXAMPLE_FILE)));
+                $keyPair = KeyPair::fromPEM($privateKeyContents, $publicKeyContents);
+            }
         });
 
         $this->specify('Loads an DH KeyPair instance from the keys PEM strings.', function () {
-            $privateKeyContents = str_replace("\r", "", trim(file_get_contents(self::DH_PRIVATE_KEY_EXAMPLE_FILE)));
-            $publicKeyContents = str_replace("\r", "", trim(file_get_contents(self::DH_PUBLIC_KEY_EXAMPLE_FILE)));
-            $keyPair = KeyPair::fromPEM($privateKeyContents, $publicKeyContents);
+            if (extension_loaded('openssl')) {
+                $privateKeyContents = str_replace("\r", "", trim(file_get_contents(self::DH_PRIVATE_KEY_EXAMPLE_FILE)));
+                $publicKeyContents = str_replace("\r", "", trim(file_get_contents(self::DH_PUBLIC_KEY_EXAMPLE_FILE)));
+                $keyPair = KeyPair::fromPEM($privateKeyContents, $publicKeyContents);
+            }
         });
 
         $this->specify('Loads an EC KeyPair instance from the keys PEM strings.', function () {
-            $privateKeyContents = str_replace("\r", "", trim(file_get_contents(self::EC_PRIVATE_KEY_EXAMPLE_FILE)));
-            $publicKeyContents = str_replace("\r", "", trim(file_get_contents(self::EC_PUBLIC_KEY_EXAMPLE_FILE)));
-            $keyPair = KeyPair::fromPEM($privateKeyContents, $publicKeyContents);
+            if (extension_loaded('openssl')) {
+                $privateKeyContents = str_replace("\r", "", trim(file_get_contents(self::EC_PRIVATE_KEY_EXAMPLE_FILE)));
+                $publicKeyContents = str_replace("\r", "", trim(file_get_contents(self::EC_PUBLIC_KEY_EXAMPLE_FILE)));
+                $keyPair = KeyPair::fromPEM($privateKeyContents, $publicKeyContents);
+            }
         });
 
         $this->specify('Detects key pairs that do not match.', function () {
-            $privateKeyContents = str_replace("\r", "", trim(file_get_contents(self::RSA_PRIVATE_KEY_EXAMPLE_FILE)));
-            $publicKeyContents = str_replace("\r", "", trim(file_get_contents(self::DSA_PUBLIC_KEY_EXAMPLE_FILE)));
-            $this->expectException(InvalidArgumentException::class);
-            $keyPair = KeyPair::fromPEM($privateKeyContents, $publicKeyContents);
+            if (extension_loaded('openssl')) {
+                $privateKeyContents = str_replace("\r", "",
+                    trim(file_get_contents(self::RSA_PRIVATE_KEY_EXAMPLE_FILE)));
+                $publicKeyContents = str_replace("\r", "", trim(file_get_contents(self::DSA_PUBLIC_KEY_EXAMPLE_FILE)));
+                $this->expectException(InvalidArgumentException::class);
+                $keyPair = KeyPair::fromPEM($privateKeyContents, $publicKeyContents);
+            }
+        });
+    }
+
+    /**
+     * Tests private/public encrypt/decrypt
+     */
+    public function testEncryptDecrypt()
+    {
+        $this->specify('Encrypts and decrypts the message correctly.', function () {
+            if (extension_loaded('openssl')) {
+                $privateKey = PrivateKey::fromPEM(file_get_contents(self::RSA_PRIVATE_KEY_EXAMPLE_FILE));
+                $publicKey = PublicKey::fromPEM(file_get_contents(self::RSA_PUBLIC_KEY_EXAMPLE_FILE));
+
+                $privateEncrypted = $privateKey->encrypt(self::PAYLOAD);
+                $this->assertEquals(self::PAYLOAD, $publicKey->decrypt($privateEncrypted));
+
+                $publicEncrypted = $publicKey->encrypt(self::PAYLOAD);
+                $this->assertEquals(self::PAYLOAD, $privateKey->decrypt($publicEncrypted));
+            }
+        });
+    }
+
+    /**
+     * Tests private/public seal/unseal
+     */
+    public function testSealUnseal()
+    {
+        $this->specify('Seals and unseals the message correctly.', function () {
+            if (extension_loaded('openssl')) {
+                $privateKey = PrivateKey::fromPEM(file_get_contents(self::RSA_PRIVATE_KEY_EXAMPLE_FILE));
+                $publicKey = PublicKey::fromPEM(file_get_contents(self::RSA_PUBLIC_KEY_EXAMPLE_FILE));
+
+                $sealed = $publicKey->seal(self::PAYLOAD);
+                $this->assertInternalType('array', $sealed);
+                $this->assertEquals(3, count($sealed));
+                $this->assertEquals(self::PAYLOAD, $privateKey->unseal($sealed[0], $sealed[1], $sealed[2]));
+            }
+        });
+    }
+
+    /**
+     * Tests private/public sign/verify
+     */
+    public function testSignVerify()
+    {
+        $this->specify('Signs and verifies the message correctly.', function () {
+            if (extension_loaded('openssl')) {
+                $privateKey = PrivateKey::fromPEM(file_get_contents(self::RSA_PRIVATE_KEY_EXAMPLE_FILE));
+                $publicKey = PublicKey::fromPEM(file_get_contents(self::RSA_PUBLIC_KEY_EXAMPLE_FILE));
+
+                $signed = $privateKey->sign(self::PAYLOAD);
+                $this->assertInternalType('array', $signed);
+                $this->assertEquals(2, count($signed));
+                $this->assertTrue($publicKey->verify(self::PAYLOAD, $signed[0], $signed[1]));
+            }
         });
     }
 }
