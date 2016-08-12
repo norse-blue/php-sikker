338bd2c687268c409cf5017fa457fc7f531b9f82
Axel Pardemann <axelitusdev@gmail.com>
2016-08-12 16:03:53 -0500

    Fixed some coding style

--- src/Asymmetric/Keys/PublicKey.php
+++ src/Asymmetric/Keys/PublicKey.php
@@ -153,8 +153,7 @@ class PublicKey extends CryptoKey
         string $message,
         string $signature,
         int $signatureAlgorithm = SignatureAlgorithm::SHA1
-    ) : bool
-    {
+    ) : bool {
         OpenSSL::resetErrors();
         if (($verified = openssl_verify($message, $signature, $this->resource, $signatureAlgorithm)) === -1) {
             // @codeCoverageIgnoreStart
@@ -186,8 +185,7 @@ class PublicKey extends CryptoKey
         }
 
         $paddedIV = InitVector::pad($iv);
-        if (@openssl_seal($message, $envelope, $envelopeKeys, [$this->resource], $cipherMethod, $paddedIV) === false
-        ) {
+        if (@openssl_seal($message, $envelope, $envelopeKeys, [$this->resource], $cipherMethod, $paddedIV) === false) {
             // @codeCoverageIgnoreStart
             throw new OpenSSLException(OpenSSL::getErrors(), 'Could not seal message.');
             // @codeCoverageIgnoreEnd
