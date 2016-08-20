c0fc8f398ed8487d9b81b1d8e9c95566d021a546
Axel Pardemann <axelitusdev@gmail.com>
2016-08-19 20:14:07 -0500

    Added the Sanitization namespace
    Added base classes for the Sanitization namespace.

---
+++ src/Sanitization/SanitizerChain.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Sikker is a PHP 7.0+ Security package that contains security related implementations.
+ *
+ * @package    NorseBlue\Sikker
+ * @version    0.3.5
+ * @author     NorseBlue
+ * @license    MIT License
+ * @copyright  2016 NorseBlue
+ * @link       https://github.com/NorseBlue/Sikker
+ */
+declare(strict_types = 1);
+
+namespace NorseBlue\Sikker\Sanitization;
+
+use Exception;
+use InvalidArgumentException;
+use RuntimeException;
+
+/**
+ * Class SanitizerChain
+ *
+ * @package NorseBlue\Sikker\Sanitization
+ * @since 0.4
+ */
+class SanitizerChain
+{
+    /**
+     * @var array Holds the sanitizers in the chain.
+     */
+    protected $sanitizers;
+
+    /**
+     * SanitizerChain constructor.
+     *
+     * @param array $sanitizers The array of sanitizers to use for the chain.
+     * @since 0.4
+     */
+    public function __construct(array $sanitizers = [])
+    {
+        $this->setSanitizers($sanitizers);
+    }
+
+    /**
+     * Gets the sanitizers.
+     *
+     * @return array Returns the array of sanitizers.
+     */
+    public function getSanitizers() : array
+    {
+        return $this->sanitizers;
+    }
+
+    /**
+     * Sets the sanitizers array.
+     *
+     * @param array $sanitizers The array of sanitizers.
+     * @return SanitizerChain Returns this instance for fluent interface.
+     * @throws InvalidArgumentException when a sanitizer is not of type Sanitizer.
+     */
+    public function setSanitizers(array $sanitizers) : SanitizerChain
+    {
+        for ($i = 0; $i < count($sanitizers); $i++) {
+            if (!($sanitizers[$i] instanceof Sanitizer)) {
+                throw new InvalidArgumentException(sprintf('Expected an item of type %s at index %d but type %s found.',
+                    Sanitizer::class, $i, gettype($sanitizers[$i])));
+            }
+        }
+        $this->sanitizers = $sanitizers;
+        return $this;
+    }
+
+    /**
+     * Runs the sanitizer chain on the given data.
+     *
+     * @param mixed $data The given data to scrub.
+     * @return mixed Returns the scrubbed data.
+     */
+    public function sweep($data)
+    {
+        $cleanData = $data;
+
+        /** @var Sanitizer $sanitizer */
+        for ($i = 0; $i < count($this->sanitizers); $i++) {
+            try {
+                $cleanData = $this->sanitizers[$i]->scrub($cleanData);
+            } catch (Exception $e) {
+                throw new RuntimeException(sprintf('Sanitizer of type %s at index %d caused an error while scrubbing the data.',
+                    gettype($this->sanitizers[$i]), $i), 0, $e);
+            }
+        }
+
+        return $cleanData;
+    }
+}
---
+++ src/Sanitization/Sanitizers/SanitizerBase.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Sikker is a PHP 7.0+ Security package that contains security related implementations.
+ *
+ * @package    NorseBlue\Sikker
+ * @version    0.3.5
+ * @author     NorseBlue
+ * @license    MIT License
+ * @copyright  2016 NorseBlue
+ * @link       https://github.com/NorseBlue/Sikker
+ */
+declare(strict_types = 1);
+
+namespace NorseBlue\Sikker\Sanitization\Sanitizers;
+
+use InvalidArgumentException;
+use NorseBlue\Sikker\Sanitization\Sanitizer;
+use RuntimeException;
+
+/**
+ * Class SanitizerBase
+ *
+ * @package NorseBlue\Sikker\Sanitization\Sanitizers
+ * @since 0.4
+ */
+abstract class SanitizerBase implements Sanitizer
+{
+    /**
+     * @var array Holds the supported types for scrubbing (bool, int, float, string, array or object).
+     */
+    const SUPPORTED_TYPES = [];
+
+    /**
+     * Determines if the sanitizer supports scrub operation on given type.
+     *
+     * @param string $type The type to check for scrub support.
+     * @return bool Returns true if the sanitizer supports scrub operation on given type, false otherwise.
+     */
+    public function isTypeSupported(string $type) : bool
+    {
+        return in_array($type, self::SUPPORTED_TYPES);
+    }
+
+    /**
+     * Determines the type of data and uses the specific scrub function to clean data.
+     *
+     * @param mixed $data The data to be scrubbed.
+     * @return mixed Returns the scrubbed data.
+     * @throws InvalidArgumentException when the given data type is not supported by the sanitizer.
+     * @throws RuntimeException when no scrub function can be executed.
+     */
+    public function scrub($data)
+    {
+        if ($data === null || is_resource($data)) {
+            return $data;
+        }
+
+        $type = gettype($data);
+        if (!$this->isTypeSupported($type)) {
+            throw new InvalidArgumentException();
+        }
+
+        $func = 'scrub'.($type == 'double') ? 'Float' : ucfirst($type);
+        if (is_callable([$this, $func])) {
+            return $this->{$func}($data);
+        }
+
+        throw new RuntimeException('Scrub operation failed due to an unexpected error.');
+    }
+
+    /**
+     * Scrubs an array data.
+     *
+     * @param array $data The data to be scrubbed.
+     * @return array Returns the scrubbed data.
+     * @throws RuntimeException when the sanitizer does not support this specific scrub operation.
+     */
+    public function scrubArray(array $data) : array
+    {
+        throw new RuntimeException(sprintf('This sanitizer does not support the %s operation.'), __FUNCTION__);
+    }
+
+    /**
+     * Scrubs a boolean data.
+     *
+     * @param bool $data The data to be scrubbed.
+     * @return bool Returns the scrubbed data.
+     * @throws RuntimeException when the sanitizer does not support this specific scrub operation.
+     */
+    public function scrubBool(bool $data) : bool
+    {
+        throw new RuntimeException(sprintf('This sanitizer does not support the %s operation.'), __FUNCTION__);
+    }
+
+    /**
+     * Scrubs a float (double) data.
+     *
+     * @param float $data The data to be scrubbed.
+     * @return float Returns the scrubbed data.
+     * @throws RuntimeException when the sanitizer does not support this specific scrub operation.
+     */
+    public function scrubFloat(float $data) : float
+    {
+        throw new RuntimeException(sprintf('This sanitizer does not support the %s operation.'), __FUNCTION__);
+    }
+
+    /**
+     * Scrubs an integer data.
+     *
+     * @param int $data The data to be scrubbed.
+     * @return int Returns the scrubbed data.
+     * @throws RuntimeException when the sanitizer does not support this specific scrub operation.
+     */
+    public function scrubInt(int $data) : int
+    {
+        throw new RuntimeException(sprintf('This sanitizer does not support the %s operation.'), __FUNCTION__);
+    }
+
+    /**
+     * Scrubs an object data.
+     *
+     * @param object $data The data to be scrubbed.
+     * @return object Returns the scrubbed data.
+     * @throws RuntimeException when the sanitizer does not support this specific scrub operation.
+     */
+    public function scrubObject(object $data) : object
+    {
+        throw new RuntimeException(sprintf('This sanitizer does not support the %s operation.'), __FUNCTION__);
+    }
+
+    /**
+     * Scrubs a string data.
+     *
+     * @param string $data The data to be scrubbed.
+     * @return string Returns the scrubbed data.
+     * @throws RuntimeException when the sanitizer does not support this specific scrub operation.
+     */
+    public function scrubString(string $data) : string
+    {
+        throw new RuntimeException(sprintf('This sanitizer does not support the %s operation.'), __FUNCTION__);
+    }
+}
